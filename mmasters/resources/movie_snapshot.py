b482ff391ad93819bd0f560fd4661711c8c2de13
Girish Verma <girish.verma026@gmail.com>
2020-06-12 16:14:32 +0530

    Girish/Ranjit: Adds api-key authentication for creating movie snapshots

--- mmasters/resources/movie_snapshot.py
+++ mmasters/resources/movie_snapshot.py
@@ -1,9 +1,23 @@
+from functools import wraps
 from http import HTTPStatus
 
+import flask_restful
+from flask import request
 from flask_restful import Resource, marshal_with, reqparse
 
 from mmasters.service.movie_snapshot_service import movie_snapshot_service
 from mmasters.view.movie_snapshot_view import movie_snapshot_view_fields
+
+
+def api_key_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        api_key = request.headers.get('x-api-key')
+        if api_key != 'abc':
+            flask_restful.abort(HTTPStatus.UNAUTHORIZED)
+        return func(*args, **kwargs)
+
+    return wrapper
 
 
 class MovieSnapshotResource(Resource):
@@ -13,6 +27,7 @@ class MovieSnapshotResource(Resource):
         self.parser.add_argument('titles', action='append', required=True, help="titles is a mandatory field")
 
     @marshal_with(fields=movie_snapshot_view_fields)
+    @api_key_required
     def post(self):
         args = self.parser.parse_args()
         movie_snapshots = movie_snapshot_service.create(args["titles"])
--- tests/resources/test_movie_snapshot.py
+++ tests/resources/test_movie_snapshot.py
@@ -6,6 +6,10 @@ from mmasters.service.movie_snapshot_service import MovieSnapshotService
 from mmasters.view.movie_snapshot_view import MovieSnapshotView
 
 
+def authorization_header():
+    return {'x-api-key': 'abc'}
+
+
 class MovieSnapshotResourceTest(unittest.TestCase):
 
     def setUp(self):
@@ -14,7 +18,9 @@ class MovieSnapshotResourceTest(unittest.TestCase):
 
     @patch("mmasters.resources.movie_snapshot.movie_snapshot_service")
     def test_should_return_status_code_as_created_when_movies_snapshots_are_created(self, movie_snapshot_service: MovieSnapshotService):
-        response = self.test_client.post("/movies-snapshots", data={"titles": ['3 Idiots', 'Dangal']})
+        response = self.test_client.post("/movies-snapshots",
+                                         json={"titles": ['3 Idiots', 'Dangal']},
+                                         headers=authorization_header())
 
         self.assertEqual(201, response.status_code)
 
@@ -22,7 +28,9 @@ class MovieSnapshotResourceTest(unittest.TestCase):
     def test_should_return_created_movie_snapshots(self, movie_snapshot_service: MovieSnapshotService):
         movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
-        response = self.test_client.post("/movies-snapshots", data={"titles": ['3 Idiots']})
+        response = self.test_client.post("/movies-snapshots",
+                                         json={"titles": ['3 Idiots']},
+                                         headers=authorization_header())
 
         expected_json = [{
             "title": "3 Idiots",
@@ -34,7 +42,9 @@ class MovieSnapshotResourceTest(unittest.TestCase):
     def test_should_create_movie_snapshots_with_titles_passed_as_payload(self, movie_snapshot_service: MovieSnapshotService):
         movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
-        self.test_client.post("/movies-snapshots", json={"titles": ['3 Idiots']})
+        self.test_client.post("/movies-snapshots",
+                              json={"titles": ['3 Idiots']},
+                              headers=authorization_header())
 
         movie_snapshot_service.create.assert_called_with(['3 Idiots'])
 
@@ -42,7 +52,9 @@ class MovieSnapshotResourceTest(unittest.TestCase):
     def test_should_return_bad_request_as_status_when_titles_is_missing_in_payload(self, movie_snapshot_service: MovieSnapshotService):
         movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
-        response = self.test_client.post("/movies-snapshots", json={})
+        response = self.test_client.post("/movies-snapshots",
+                                         json={},
+                                         headers=authorization_header())
 
         self.assertEqual(400, response.status_code)
 
@@ -51,7 +63,9 @@ class MovieSnapshotResourceTest(unittest.TestCase):
                                                                                    movie_snapshot_service: MovieSnapshotService):
         movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
-        response = self.test_client.post("/movies-snapshots", json={'titles': []})
+        response = self.test_client.post("/movies-snapshots",
+                                         json={'titles': []},
+                                         headers=authorization_header())
 
         self.assertEqual(400, response.status_code)
 
@@ -60,7 +74,17 @@ class MovieSnapshotResourceTest(unittest.TestCase):
                                                                                    movie_snapshot_service: MovieSnapshotService):
         movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
-        response = self.test_client.post("/movies-snapshots", json={})
+        response = self.test_client.post("/movies-snapshots",
+                                         json={},
+                                         headers=authorization_header())
 
         expected_message = {'message': {'titles': 'titles is a mandatory field'}}
         self.assertEqual(expected_message, response.get_json())
+
+    @patch("mmasters.resources.movie_snapshot.movie_snapshot_service")
+    def test_should_return_unauthorized_as_status_code_when_api_key_is_missing_in_header(self,
+                                                                                       movie_snapshot_service: MovieSnapshotService):
+        response = self.test_client.post("/movies-snapshots", json={"titles": ['3 Idiots']})
+
+        self.assertEqual(401, response.status_code)
+
