3bcdd76321e4051d3cfe6b118a1b8446eb7e6507
Girish Verma <girish.verma026@gmail.com>
2020-06-13 17:00:12 +0530

    Girish/Ranjit: Refactors saving of movie snapshot into a separate function

--- mmasters/service/movie_snapshot_service.py
+++ mmasters/service/movie_snapshot_service.py
@@ -3,20 +3,22 @@ from typing import List
 from mmasters.client.movie_client import movie_client
 from mmasters.entity.movie_snapshot import MovieSnapshot
 from mmasters.repository.movie_snapshot_repository import movie_snapshot_repository
+from mmasters.view.movie_snapshot_view import MovieSnapshotView
 
 
 class MovieSnapshotService:
 
-    def create(self, titles: List[str]):
+    def create(self, titles: List[str]) -> List[MovieSnapshotView]:
         movies = [movie_client.fetch_movies(title) for title in titles]
-        movie_snapshots_view = []
-        for movie in movies:
+
+        def __save(movie) -> MovieSnapshotView:
             movie_snapshot = MovieSnapshot.of(movie)
             movie_snapshot_repository.save(movie_snapshot)
-            movie_snapshots_view.append(movie_snapshot.to_snapshot_view())
-        return movie_snapshots_view
+            return movie_snapshot.to_snapshot_view()
 
-    def get_all(self):
+        return [__save(movie) for movie in movies]
+
+    def get_all(self) -> List[MovieSnapshotView]:
         movie_snapshots = movie_snapshot_repository.find_all()
         return [movie_snapshot.to_snapshot_view() for movie_snapshot in movie_snapshots]
 
