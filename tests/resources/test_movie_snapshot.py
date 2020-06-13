7506ac771235fc40c7d57d10b550a8d5b3b52c1d
Girish Verma <girish.verma026@gmail.com>
2020-06-13 11:41:54 +0530

    Girish/Ranjit: Refactors unit test to create patch in setup method

--- tests/resources/test_movie_snapshot.py
+++ tests/resources/test_movie_snapshot.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import patch
 
 from mmasters.app import Application
-from mmasters.service.movie_snapshot_service import MovieSnapshotService
 from mmasters.view.movie_snapshot_view import MovieSnapshotView
 from tests.config.test_config import TestConfig
 
@@ -16,18 +15,21 @@ class MovieSnapshotResourceTest(unittest.TestCase):
     def setUp(self):
         self.app = Application.create_app(TestConfig)
         self.test_client = self.app.test_client()
+        self.movie_snapshot_service_patch = patch("mmasters.resources.movie_snapshot_resource.movie_snapshot_service")
+        self.movie_snapshot_service = self.movie_snapshot_service_patch.start()
 
-    @patch("mmasters.resources.movie_snapshot_resource.movie_snapshot_service")
-    def test_should_return_status_code_as_created_when_movies_snapshots_are_created(self, movie_snapshot_service: MovieSnapshotService):
+    def tearDown(self) -> None:
+        self.movie_snapshot_service_patch.stop()
+
+    def test_should_return_status_code_as_created_when_movies_snapshots_are_created(self):
         response = self.test_client.post("/movies-snapshots",
                                          json={"titles": ['3 Idiots', 'Dangal']},
                                          headers=authorization_header())
 
         self.assertEqual(201, response.status_code)
 
-    @patch("mmasters.resources.movie_snapshot_resource.movie_snapshot_service")
-    def test_should_return_created_movie_snapshots(self, movie_snapshot_service: MovieSnapshotService):
-        movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
+    def test_should_return_created_movie_snapshots(self):
+        self.movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
         response = self.test_client.post("/movies-snapshots",
                                          json={"titles": ['3 Idiots']},
@@ -39,19 +41,17 @@ class MovieSnapshotResourceTest(unittest.TestCase):
         }]
         self.assertEqual(expected_json, response.get_json())
 
-    @patch("mmasters.resources.movie_snapshot_resource.movie_snapshot_service")
-    def test_should_create_movie_snapshots_with_titles_passed_as_payload(self, movie_snapshot_service: MovieSnapshotService):
-        movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
+    def test_should_create_movie_snapshots_with_titles_passed_as_payload(self):
+        self.movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
         self.test_client.post("/movies-snapshots",
                               json={"titles": ['3 Idiots']},
                               headers=authorization_header())
 
-        movie_snapshot_service.create.assert_called_with(['3 Idiots'])
+        self.movie_snapshot_service.create.assert_called_with(['3 Idiots'])
 
-    @patch("mmasters.resources.movie_snapshot_resource.movie_snapshot_service")
-    def test_should_return_bad_request_as_status_when_titles_is_missing_in_payload(self, movie_snapshot_service: MovieSnapshotService):
-        movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
+    def test_should_return_bad_request_as_status_when_titles_is_missing_in_payload(self):
+        self.movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
         response = self.test_client.post("/movies-snapshots",
                                          json={},
@@ -59,10 +59,8 @@ class MovieSnapshotResourceTest(unittest.TestCase):
 
         self.assertEqual(400, response.status_code)
 
-    @patch("mmasters.resources.movie_snapshot_resource.movie_snapshot_service")
-    def test_should_return_bad_request_as_status_when_titles_is_empty_list_in_payload(self,
-                                                                                   movie_snapshot_service: MovieSnapshotService):
-        movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
+    def test_should_return_bad_request_as_status_when_titles_is_empty_list_in_payload(self):
+        self.movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
         response = self.test_client.post("/movies-snapshots",
                                          json={'titles': []},
@@ -70,10 +68,8 @@ class MovieSnapshotResourceTest(unittest.TestCase):
 
         self.assertEqual(400, response.status_code)
 
-    @patch("mmasters.resources.movie_snapshot_resource.movie_snapshot_service")
-    def test_should_return_error_message_in_response_when_titles_is_missing_in_payload(self,
-                                                                                   movie_snapshot_service: MovieSnapshotService):
-        movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
+    def test_should_return_error_message_in_response_when_titles_is_missing_in_payload(self):
+        self.movie_snapshot_service.create.return_value = [MovieSnapshotView('3 Idiots', "2009")]
 
         response = self.test_client.post("/movies-snapshots",
                                          json={},
@@ -82,9 +78,7 @@ class MovieSnapshotResourceTest(unittest.TestCase):
         expected_message = {'message': {'titles': 'titles is a mandatory field'}}
         self.assertEqual(expected_message, response.get_json())
 
-    @patch("mmasters.resources.movie_snapshot_resource.movie_snapshot_service")
-    def test_should_return_unauthorized_as_status_code_when_api_key_is_missing_in_header(self,
-                                                                                       movie_snapshot_service: MovieSnapshotService):
+    def test_should_return_unauthorized_as_status_code_when_api_key_is_missing_in_header(self):
         response = self.test_client.post("/movies-snapshots", json={"titles": ['3 Idiots']})
 
         self.assertEqual(401, response.status_code)
