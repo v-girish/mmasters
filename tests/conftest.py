9a9e662c7e57501c5e0bf84dadeeab39f7bc65d7
Ranjit Shinde <ranjit.shinde.thoughtworks@aegonlife.com>
2020-06-16 12:09:55 +0530

    Girish/Ranjit: Exports flask app and db url as environment variable in test to run db migrations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 
@@ -12,11 +13,18 @@ def pytest_unconfigure():
 def setup_database():
     print("Bringing up database...")
     subprocess.call(["docker-compose", "-f", "tests/docker-compose.yaml", "up", "-d"])
+
     subprocess.call(["sleep", "5"])
     subprocess.call(["docker", "exec", "postgres", "createdb", "-U", "postgres", "mmasters"])
+
+    print("setting up database url as environment variable")
+    os.environ["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:secret@localhost:5432/mmasters"
+
+    print("setting up flask app as environment variable")
+    os.environ["FLASK_APP"] = "run_app.py"
+
     print("Running migrations...")
     subprocess.call(["flask", "db", "upgrade"])
-
 
 
 def shutdown_database():
