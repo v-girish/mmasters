73af92b3099c945ec84f9225a0cd28b3a754b8d1
Girish Verma <girish.verma026@gmail.com>
2020-06-17 12:59:43 +0530

    Girish/Ranjit: Removes exceptions from movie response

--- mmasters/client/response/movie_response.py
+++ mmasters/client/response/movie_response.py
@@ -4,7 +4,6 @@ from http import HTTPStatus
 import requests
 
 from mmasters.client.model.movie import Movie, EmptyMovie
-from mmasters.exception.exception import MovieClientException, MovieNotFoundException
 
 
 class MovieResponse:
@@ -15,23 +14,13 @@ class MovieResponse:
         self.logger = logging.getLogger(__name__)
 
     def movie(self) -> Movie:
-        try:
-            self.__parse()
-        except Exception:
-            self.logger.exception(f"Error while fetching movie with title {self.__title}")
+        self.logger.info(f"Received status code for movie {self.__title} as {self.__response.status_code}")
+
+        if self.__is_not_success_response() or self.__is_not_found_response():
+            self.logger.error(f"Received error response for movie {self.__title}: {self.__response.text}")
             return EmptyMovie(self.__title)
 
         return Movie.from_json(self.__response.json())
-
-    def __parse(self):
-        self.logger.info(f"Received status code for movie {self.__title} as {self.__response.status_code}")
-
-        if self.__is_not_success_response():
-            self.logger.error(f"Received error response for movie {self.__title}: {self.__response.text}")
-            raise MovieClientException("Something went wrong")
-
-        if self.__is_not_found_response():
-            raise MovieNotFoundException(self.__title)
 
     def __is_not_success_response(self) -> bool:
         return self.__response.status_code != HTTPStatus.OK
